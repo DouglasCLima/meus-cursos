6d5429dc0fd17a0918493ecc95768bb105f14163
DouglasCLima <143197856+DouglasCLima@users.noreply.github.com>
2025-04-10 13:24:06 -0300

    Update Curso.java

--- src/main/java/org/acme/model/Curso.java
+++ src/main/java/org/acme/model/Curso.java
@@ -4,11 +4,12 @@ import io.quarkus.hibernate.orm.panache.PanacheEntity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
-import jakarta.validation.constraints.NotBlank;
 import org.eclipse.microprofile.openapi.annotations.media.Schema;
-import org.joda.time.LocalDate;
+import java.time.LocalDate;
 
-import javax.validation.constraints.NotNull;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.Valid;
 import java.util.List;
 
 @Entity
